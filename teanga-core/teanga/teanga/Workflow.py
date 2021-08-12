c33e0fa859f501ef76ebacd8287716bb4414b9bc
evadebarros01 <evadebarros05@gmail.com>
2021-08-12 11:58:30 +0100

    removed unnecesary stop operator

--- teanga-core/teanga/teanga/Workflow.py
+++ teanga-core/teanga/teanga/Workflow.py
@@ -220,10 +220,6 @@ class Workflow:
                        compose_operator_ >> pre_operator 
                 #}}
 
-        #for workflow_step, step_description in list(self.workflow.items()):        
-        #   if not step_description["dependencies"]: 
-        #        for rq_operators_instances in rq_operators_instances:  
-        #                rq_operators_instances >> stop_operators_instances 
                 
 
            pre_operator.op_kwargs.update({"given_inputs":given_inputs,
