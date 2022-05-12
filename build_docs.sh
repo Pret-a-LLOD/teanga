7a770667e9eb61121d9dd35ed84249497ccd5b32
berstearns <bernardo.stearns@gmail.com>
2022-05-12 11:49:01 +0200

    docs

--- build_docs.sh
+++ build_docs.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 rm -rf ./docs
 cd ./docs-generator
-sphinx-build . build
-mv -i build/_static build/static
-cd ./build
-for f in `ls . | grep '.html'`;
-do 
-    echo $f
-    sed -i -e 's/_static/static/g' $f
-done
-cd ..
-mv -i build ../docs
-touch ../docs/.nojekyll
-cd ..
+python3 -m mkdocs build
